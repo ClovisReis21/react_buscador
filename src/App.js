0d8e1c90794e6aef40cfd5adbe7d2323c1c3a8d2
ClovisReis21 <clovis.fmu@live.com>
2022-07-05 14:09:01 -0300

    added input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1>TESTE PROJETO</h1>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerinput">
+        <input
+          type="text"
+          placeholder="Digite seu cep..."
+        />
+      </div>
     </div>
   );
 }
