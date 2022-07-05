063a5eac71500dd2b9c0aff92f1f7a63f7c08418
ClovisReis21 <clovis.fmu@live.com>
2022-07-05 14:39:04 -0300

    ogj to render

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import { FiSearch } from 'react-icons/fi';
 function App() {
   return (
     <div className="container">
@@ -9,6 +9,21 @@ function App() {
           type="text"
           placeholder="Digite seu cep..."
         />
+
+        <button className="buttonSearsh">
+          <FiSearch zise={25} color="#000"/>
+        </button>
+
+        <main className='main'>
+          <h2>CEP: 05574-440</h2>
+
+          <span>Rua teste</span>
+          <span>Complemento: Casa 2</span>
+          <span>Bairro Bom</span>
+          <span>Manaus - AM</span>
+
+        </main>
+
       </div>
     </div>
   );
